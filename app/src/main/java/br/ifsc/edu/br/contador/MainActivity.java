704ae8e77a3e44b092b0f4d53ff4cfb6a9968e04
Victor Mendes <victorgomesmendes05@gmail.com>
2024-09-09 22:07:29 -0300

    contador com limite definido

--- app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/br/contador/MainActivity.java
@@ -5,23 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    int contador = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button mybutton = findViewById(R.id.button);
-        mybutton.setText("0");
+        TextView myTextView = findViewById(R.id.textView);
+        EditText edMin = findViewById(R.id.edMin);
+        EditText edMax = findViewById(R.id.edMax);
+
         mybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Button b=(Button) view;
-                b.setText(Integer.toString(contador));
-                contador++;
+                TextView t=(TextView) view;
+                Random random= new Random();
+                int nmin = (Integer.parseInt(edMin.getText().toString()));
+                int nmax = (Integer.parseInt(edMax.getText().toString()));
+                myTextView.setText(Integer.toString(random.nextInt(nmax-nmin+1)+nmin));
             }
         });
     }
